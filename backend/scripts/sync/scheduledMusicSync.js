fc2c7624d46c988ef075a832becb39001bdd6333
Chris Horn <23chrish@gmail.com>
2025-09-08 20:35:15 +0100

    update paths on spotify sync script

--- backend/scripts/sync/scheduledMusicSync.js
+++ backend/scripts/sync/scheduledMusicSync.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import dotenv from 'dotenv';
-import { initializeDatabase } from '../src/db/db.js';
-import MusicSyncService from '../src/services/musicSync.js';
+import { initializeDatabase } from '../../src/db/db.js';
+import MusicSyncService from '../../src/services/musicSync.js';
 import logger from '../../src/utils/logger.js';
 
 // Load environment variables
